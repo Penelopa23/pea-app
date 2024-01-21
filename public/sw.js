1aeb1a43735207999c12f517ebe396556f722dff
Artem <timoseyka@gmail.com>
2024-01-22 02:18:53 +0300

    Added some code

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,30 @@
-console.warn("sw file is public folder")
+
+
+let cacheData = "appV1"
+this.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(cacheData).then((cache) => {
+            cache.addAll([
+                '/static/js/bundle.js',
+                '/index.html',
+                '/manifest.json',
+                '/users',
+                '/about',
+                '/logo192.png',
+                '/favicon.ico',
+                '/'
+            ])
+        })
+    )
+})
+
+
+this.addEventListener("cacheFetch", (event) => {
+    event.respondWith(
+        caches.match(event.request ).then((response) => {
+            if (response) {
+                return response
+            }
+        })
+    )
+})
--- src/swDev.js
+++ src/swDev.js
@@ -2,8 +2,6 @@
 
 export default function swDev() {
     let swUrl = `${process.env.PUBLIC_URL}/sw.js`
-    //let swUrl = '%PUBLIC_URL%/sw.js';
-    console.log(`${process.env.PUBLIC_URL}`)
     navigator.serviceWorker.register(swUrl).then((response) => {
         console.warn("response", response)
     }).catch((e) => {
